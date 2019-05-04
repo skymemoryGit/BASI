982e6859ed2e978bac43fb829486e33b9573db34
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-04 18:07:42 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 	//print_r(PDO::getAvailableDrivers());
 	$dbname='yejianchen';	
-	$dbusername='yejianchen';	
-	$dbpassword='Random1996!';		
+	$dbusername='webdb';	
+	$dbpassword='webdb';		
 	
 	// Connessione al database
 	//Nuova istanza della classe PDO
