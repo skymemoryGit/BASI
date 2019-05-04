6140d1e453498b0a9f79472bb9a30dc1faaad159
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-04 19:40:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     <head>
         <title>PDO Read Records - code from codeofaninja.com</title>
   
-  
-  
-  </head>
+    </head>
 
 
 <body>
@@ -13,15 +11,12 @@
 <h1>PDO: Read Records</h1>
   
 
-
-
 <?php
 
 //include database connection
 include 'db_connect.php';
   
 //select all data
-
 $query = "SELECT matricola,cognome,nome FROM studente ;
 $result = pg_query($query);
 if (!$result) {
@@ -31,8 +26,8 @@ if (!$result) {
 }
 
 $myrow = pg_fetch_assoc($result); 
-$value == $myrow[rating];
-$value2 == $myrow[numofratings];
+$value == $myrow[matricola];
+$value2 == $myrow[cognome];
 
 echo "$value, $value2";
 
@@ -44,69 +39,6 @@ echo "$value, $value2";
 
 
 
-/*
-$query = "SELECT matricola,nome,cognome FROM studente";
 
-$stmt = $con-> prepare( $query );
-$stmt->execute();
-
-
-$num = $stmt->rowCount();
-
-echo "il numero della tab :".$num ;
-
-
-
-if($num>0){
-  
-    //start table
-    echo "<table border='2'>";
-  
-        //creating our table heading
-        echo "<tr>";
-            echo "<th>matricola</th>";
-            echo "<th>nome</th>";
-            echo "<th>cognome</th>";
-          //  echo "<th>Action</th>";
-        echo "</tr>";
-  
-        //retrieve our table contents
-        //fetch() is faster than fetchAll()
-        //http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            //extract row
-            //this will make $row['firstname'] to
-            //just $firstname only
-            extract($row);
-  
-            //creating new table row per record
-            echo "<tr>";
-                echo "<td>{$matricola}</td>";
-                echo "<td>{$nome}</td>";
-                echo "<td>{$cognome}</td>";
-                echo "<td>";
-                    //we will use this links on next part of this post
-                    echo "<a href='edit.php?id={$id}'>Edit</a>";
-                    echo " / ";
-                    //we will use this links on next part of this post
-                    echo "<a href='#' onclick='delete_user( {$id} );'>Delete</a>";
-                echo "</td>";
-            echo "</tr>";
-        }
-  
-    //end table
-    echo "</table>";
-  
-}
-  
-//if no records found
-else{
-    echo "No records found.";
-}
-?> 
-<!-- dynamic content will be here -->
- 
-
-*/
 </body>
 </html>
