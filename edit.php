10f44f6750e670397fd98b5b588ca25851887ccd
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-08 12:49:19 +0200

    Add files via upload

--- edit.php
+++ edit.php
@@ -18,19 +18,18 @@ if($_POST){
         //in this case, it seemed like we have so many fields to pass and
         //its kinda better if we'll label them and not use question marks
         //like what we used here
-        $query = "update ye_isuru
-                    set firstname = :firstname, lastname = :lastname, username = :username, password  = :password
-                    where id = :id";
+        $query = "update studente
+                    set matricola = :matricola, nome = :nome, cognome = :cognome
+                    where matricola = :matricola";
   
         //prepare query for excecution
-        $stmt = $con->prepare($query);
+        $stmt = $db->prepare($query);
   
         //bind the parameters
-        $stmt->bindParam(':firstname', $_POST['firstname']);
-        $stmt->bindParam(':lastname', $_POST['lastname']);
-        $stmt->bindParam(':username', $_POST['username']);
-        $stmt->bindParam(':password', $_POST['password']);
-        $stmt->bindParam(':id', $_POST['id']);
+        $stmt->bindParam(':matricola', $_POST['matricola']);
+        $stmt->bindParam(':nome', $_POST['nome']);
+        $stmt->bindParam(':cognome', $_POST['cognome']);
+        
   
         // Execute the query
         if($stmt->execute()){
@@ -50,11 +49,11 @@ if($_POST){
 try {
   
     //prepare query
-    $query = "select id, firstname, lastname, username, password from ye_isuru where id = ? limit 0,1";
-    $stmt = $con->prepare( $query );
+    $query = "select matricola, nome, cognome from studente where matricola = ? ";
+    $stmt = $db->prepare( $query );
   
     //this is the first question mark
-    $stmt->bindParam(1, $_REQUEST['id']);
+    $stmt->bindParam(1,$_REQUEST['matricola']);
   
     //execute our query
     $stmt->execute();
@@ -63,11 +62,10 @@ try {
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
   
     //values to fill up our form
-    $id = $row['id'];
-    $firstname = $row['firstname'];
-    $lastname = $row['lastname'];
-    $username = $row['username'];
-    $password = $row['password'];
+    $matricola = $row['matricola'];
+    $nome = $row['nome'];
+    $cognome= $row['cognome'];
+    
   
 }catch(PDOException $exception){ //to handle error
     echo "Error: " . $exception->getMessage();
@@ -75,29 +73,23 @@ try {
 ?>
 
 
-<!--we have our html form here where new user information will be entered-->
+!--we have our html form here where new user information will be entered-->
 <form action='#' method='post' border='0'>
     <table>
         <tr>
-            <td>Firstname</td>
-            <td><input type='text' name='firstname' value='<?php echo $firstname;  ?>' /></td>
+            <td>nome</td>
+            <td><input type='text' name='nome' value='<?php echo $nome;  ?>' /></td>
         </tr>
         <tr>
-            <td>Lastname</td>
-            <td><input type='text' name='lastname' value='<?php echo $lastname;  ?>' /></td>
+            <td>cognome</td>
+            <td><input type='text' name='cognome' value='<?php echo $cognome;  ?>' /></td>
         </tr>
-        <tr>
-            <td>Username</td>
-            <td><input type='text' name='username'  value='<?php echo $username;  ?>' /></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-            <td><input type='password' name='password'  value='<?php echo $password;  ?>' /></td>
-        <tr>
+        
+        
             <td></td>
             <td>
                 <!-- so that we could identify what record is to be updated -->
-                <input type='hidden' name='id' value='<?php echo $id ?>' />
+                <input type='hidden' name='matricola' value='<?php echo $matricola ?>' />
  
                 <input type='submit' value='Edit' />
   
